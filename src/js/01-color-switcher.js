81ca30c48bf8a988d149e0cce6b438e20768e869
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-26 10:25:45 +0300

    task - 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  timerId = setInterval(onButtonStart, 1000);
+});
+
+function onButtonStart() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
