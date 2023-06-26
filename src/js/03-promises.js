fba21eb824608ecce748660af9485ebd1fcbd622
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-26 09:57:57 +0300

    task - 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+const inputDelayEl = document.querySelector('input[name="delay"]');
+const inputStepEl = document.querySelector('input[name="step"]');
+const inputAmountEl = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+formEl.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  let delay = Number(inputDelayEl.value);
+  let step = Number(inputStepEl.value);
+  let amount = Number(inputAmountEl.value);
+  let position = 0;
+  delay = delay - step;
+  formEl.reset();
+  for (let i = 0; i < amount; i += 1) {
+    position = i + 1;
+    delay += step;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
